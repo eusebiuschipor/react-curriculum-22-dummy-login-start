18a5210f6e6ee949bf59b572f0e2c0bdd1e705bf
eusebiuschipor <eusebiu@eusebiuschipor.com>
2021-09-18 08:36:54 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+
+function App() {
+
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+
+import Card from '../UI/Card/Card';
+import classes from './Login.module.css';
+import Button from '../UI/Button/Button';
+
+const Login = (props) => {
+  
+  return (
+    <Card className={classes.login}>
+      
+    </Card>
+  );
+};
+
+export default Login;
